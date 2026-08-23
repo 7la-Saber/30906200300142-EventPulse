fb7f6367e275691dd3942a8a67f2f10e69642375
Your Name <your-email@example.com>
2026-08-23 12:40:31 +0300

    fix: switch to swagger.json to fix vercel crash

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const http = require('http');
 const { Server } = require('socket.io');
 const Message = require('./models/Message');
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
 const path = require('path');
-const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
+const swaggerDocument = require('./swagger.json');
 const mongoose = require('mongoose');
 const errorHandler = require('./middleware/errorMiddleware');
 
