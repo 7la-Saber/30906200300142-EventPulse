23be91e5db8c13569388916c5f3c5f9407857a09
Your Name <your-email@example.com>
2026-08-23 12:48:21 +0300

    fix: disable server.listen on vercel environment

--- server.js
+++ server.js
@@ -74,8 +74,10 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
 module.exports = app;
